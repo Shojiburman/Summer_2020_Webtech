01569ed27178a2aa52fcc5ce6325ba18a7bbf8e1
unknown <shojiburman@gmail.com>
2020-07-23 23:59:05 +0600

    Email field validation

--- lab-3/index.php
+++ lab-3/index.php
@@ -49,10 +49,86 @@
             }
         }
 
+        if (isset($_POST['emailSubmit'])) {
+            if (isset($_POST['email'])) {
+                $email = trim($_POST['email']);
+                if (!$email == '') {
+                    if (substr_count($email, ' ') == 0) {
+                        if (substr_count($email, '@') == 0) {
+                            $emailErr = 'Email must have "@"';
+                        } else if (substr_count($email, '@') == 1) {
+                            if (strpos($email, '@') != 0) {
+                                if (substr_count($email, '.') != 0) {
+                                    $atpos = strpos($email, '@');
+                                    $domainPart = substr($email, $atpos);
+
+                                    $dotpos = strrpos($domainPart, '.');
+                                    $domainExt = substr($domainPart, $dotpos);
+                                    $domainName = str_replace($domainExt,"", $domainPart);
+
+                                    if (strlen($domainName) > 0 && validateDomainName($domainName)) {
+                                        if (strlen($domainExt) > 1 && validateDomainExt($domainExt)) {} else {
+                                            $emailErr = 'Email must have more than 1 letter and letters only after last "."';
+                                        }
+                                    } else {
+                                        $emailErr = 'Email can only have dot(.), dash(-), chracters and numbers between "@" and last "." with no adjacent "@" or "."';
+                                    }
+                                } else {
+                                    $emailErr = 'Email must have "."';
+                                }
+                            } else {
+                                $emailErr = 'Email can not start with "@"';
+                            }                   
+                        } else {
+                            $emailErr = 'Email can not have multiple "@"';
+                        } 
+                    } else {
+                        $emailErr = 'Email can not have spaces';
+                    }
+                } else {
+                    $emailErr = 'Email can not be empty';
+                }
+            } else {
+                $emailErr = 'Email is required';
+            }
+        }
+
         function validateName($string) {
             $array = str_split($string);
             foreach ($array as $value) {
                 if ($value == '.' || $value == '-' || $value == ' ' || ctype_alpha($value)) {
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        function validateDomainName($string) {
+            foreach (explode(".", $string) as $part) {
+                if ($part == '') {
+                    return false;
+                }
+            }
+            $array = str_split($string);
+            foreach ($array as $value) {
+                if ($value == '-' || $value == '.' || is_numeric($value) || ctype_alpha($value)) {
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        function validateDomainExt($string) {
+            if (is_numeric($string[0])) {
+                return false;
+            }
+            $array = str_split($string);
+            foreach ($array as $value) {
+                if (is_numeric($value) || ctype_alpha($value)) {
                     continue;
                 } else {
                     return false;
@@ -67,6 +143,99 @@
         <strong><span> <?php echo $nameErr;?></span></strong><br>
         <input name="nameSubmit" type="submit" value="Submit"><br><br>
     </form>
-    
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+        <label>Email</label><br>        
+        <input type="text" name="email" value="<?php echo $email;?>" style="margin: 5px 0px 5px 0px">
+        <img src="info.png" width="17px" height="17px" alt="alternative text" title="Hint: sample@example.com">
+        <strong><span> <?php echo $emailErr;?></span></strong><br>
+        <input name="emailSubmit" type="submit" value="Submit"><br><br>
+    </form>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+        <fieldset style="width: 200px; display: inline-block;">
+            <legend>Gender</legend>
+            <input type="radio" name="gender" value="Male"><label>Male</label>
+            <input type="radio" name="gender" value="Female"><label>Female</label>
+            <input type="radio" name="gender" value="Other"><label>Other</label>
+        </fieldset>
+        <strong><span><?php echo $genderErr;?></span></strong><br>
+        <input name="genderSubmit" type="submit" value="Submit"><br><br>
+    </form>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+        <table>
+            <tr>
+                <td>
+                    <fieldset style="width: auto; display: inline-block;">
+                        <legend>Date of birth</legend>
+                            <table>
+                                <tr>
+                                    <td align="center">dd</td>
+                                    <td></td>
+                                    <td align="center">mm</td>
+                                    <td></td>
+                                    <td align="center">yyyy</td>
+                                </tr>
+                                <tr>
+                                    <td style="padding-bottom: 10px"><input min="1" max="31" type="number" name="date" value="<?php echo $date;?>" placeholder="" size="1px"></td>
+                                    <td style="padding-bottom: 10px"><strong style="color: black"> / </strong></td>
+                                    <td style="padding-bottom: 10px"><input min="1" max="12" type="number" name="month" value="<?php echo $month;?>" placeholder="" size="1px"></td>
+                                    <td style="padding-bottom: 10px"><strong style="color: black"> / </strong></td>
+                                    <td style="padding-bottom: 10px"><input min="1990" max="2016" type="number" name="year" value="<?php echo $year;?>" placeholder="" size="2px"></td>
+                                </tr>
+                                <tr><td colspan="5" style="border-top: 1px solid black; padding-top: 10px"><input name="dobSubmit" type="submit" value="Submit"></td></tr>
+                            </table>
+                    </fieldset>
+                </td>
+                <td>
+                    <strong><span><?php echo $dateErr;?></span></strong>
+                </td>
+            </tr>
+        </table>
+    </form>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+        <fieldset style="width: 200px; display: inline-block;">
+            <legend>Degree</legend>
+            <input type="checkbox" name="degree" value="SSC"><label>SSC</label>
+            <input type="checkbox" name="degree" value="HSC"><label>HSC</label>
+            <input type="checkbox" name="degree" value="BSc"><label>BSc</label>
+        </fieldset>
+        <strong><span><?php echo $degreeErr;?></span></strong><br>
+        <input name="degreeSubmit" type="submit" value="Submit"><br><br>
+    </form>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+        <div style="border-bottom: 1px solid black; padding-bottom: 10px; margin-bottom: 10px; display: inline-block;">
+            <label>Blood Group</label>
+            <select name="blood">
+                <option value="A+">A+</option>
+                <option value="A-">A-</option>
+                <option value="B+">B+</option>
+                <option value="B-">B-</option>
+                <option value="AB+">AB+</option>
+                <option value="AB-">AB-</option>
+                <option value="O+">O+</option>
+                <option value="O-">O-</option>
+            </select>
+        </div>
+        <strong><span><?php echo $bloodErr;?></span></strong><br>
+        <input name="bloodSubmit" type="submit" value="Submit"><br><br>
+    </form>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+    <table>
+        <tr>
+            <td>
+                <fieldset style="width: 200px; display: inline-block;">
+                    <legend>Profile Picture</legend>
+                    <label>User ID</label>
+                    <input type="number" name="uid" value="<?php echo $uid;?>" min="0"><br>
+                    <label>Picture</label>
+                    <input type="file" name="pic"><br>
+                    <input name="profileSubmit" type="submit" value="Submit">
+                </fieldset>
+            </td>
+            <td>
+                <strong><span><?php echo $picErr;?></span></strong><br>
+            </td>
+        </tr>
+    </table>
+    </form>
 </body>
 </html>
