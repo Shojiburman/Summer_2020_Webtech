c121cc9188f971a39bd2956eedf3d0c3b3980107
unknown <shojiburman@gmail.com>
2020-07-28 21:53:35 +0600

    Designed pages structure (HTML and CSS only)

---
+++ lab-5/home.php
@@ -0,0 +1,23 @@
+<?php
+	session_start();
+	if($_SESSION['status'] == "Ok"){ 
+
+?>
+	
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+
+	<h1>Welcome home!</h1> <a href="logout.php"> Logout</a>
+</body>
+</html>
+
+
+<?php
+	}else{
+		header('location: login.html');
+	}
+?>
