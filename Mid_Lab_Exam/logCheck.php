8aa65a9b101f9d638b0ff008aa901ae6e1af6dc1
unknown <shojiburman@gmail.com>
2020-08-04 19:52:11 +0600

    multiple users login

--- Mid_Lab_Exam/logCheck.php
+++ Mid_Lab_Exam/logCheck.php
@@ -3,26 +3,52 @@
 
 	if(isset($_POST['submit'])){
 
-		$uname 		= $_POST['name'];
+		$name 		= $_POST['name'];
 		$password 	= $_POST['password'];
 
-		if(empty($uname) || empty($password)){
+		if(empty($name) || empty($password)){
 			echo "null submission";
 
-		}else{
-			
+		} else{
 			$file = fopen('user.txt', 'r');
 			$data = fread($file, filesize('user.txt'));
-			$user = explode('|', $data);
+			//$user = explode('|', $data);
+
+				$userData = explode("|",$data);
+
+				$i = 0;
+				foreach ($userData as $us) {
+				 	if(trim($us) == $name){
+				 		if(trim($userData[$i+1]) == $password){
+							$_SESSION['status']  = "Ok";
+							header('location: home.php');
+						}
+				}else{
+					echo "Invalid username/password";
+				}
+				$i++;
+				} 
+				/*if(trim($userData[0]) == $name && trim($userData[1]) == $password){
+					$_SESSION['status']  = "Ok";
+					header('location: home.php');
+				}else{
+					echo "Invalid username/password";
+				}*/
 
 
+			/*while(!feof($data)){
+				$line = fgets($data);
+				$user = explode('|', $line);
 
-			if(trim($user[0]) == $uname && trim($user[1]) == $password){
-				$_SESSION['status']  = "Ok";
-				header('location: home.php');
-			}else{
-				echo "Invalid username/password";
-			}
+				if(trim($user[0]) == $name && trim($user[1]) == $password){
+					$_SESSION['status']  = "Ok";
+					header('location: home.php');
+				}else{
+					echo "Invalid username/password";
+				}
+			}*/
+
+			fclose($file);
 		}
 
 	}else{
--- Mid_Lab_Exam/regCheck.php
+++ Mid_Lab_Exam/regCheck.php
@@ -15,7 +15,7 @@
 		}else {
 
 			$file = fopen('user.txt', 'a');
-			fwrite($file, $name.'|'.$password.'|'.$email.'|'.$id."/r/n");
+			fwrite($file, $name.'|'.$password."\r\n");
 			fclose($file);
 
 			header('location: login.html');
