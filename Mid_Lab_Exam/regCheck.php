7efabd48f7557aea4fe970db3e9f1c6a87538d44
unknown <shojiburman@gmail.com>
2020-08-04 08:55:32 +0600

    registration

---
+++ Mid_Lab_Exam/regCheck.php
@@ -0,0 +1,29 @@
+  
+<?php
+	session_start();
+
+	if(isset($_POST['submit'])){
+
+
+		$id = $_POST['id'];
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+
+		if(empty($name) || empty($id) || empty($password) || empty($email)){
+			echo "null submission";
+		}else {
+
+			$file = fopen('user.txt', 'a');
+			fwrite($file, $id.'|'.$name.'|'.$password.'|'.$email."/r/n");
+			fclose($file);
+
+			header('location: login.html');
+		}
+
+	}else{
+		header("location: login.html");
+	}
+
+
+?>
