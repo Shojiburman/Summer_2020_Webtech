0234ec8a413b3f16940a0a584156c6032a66b9e2
unknown <shojiburman@gmail.com>
2020-08-15 15:49:36 +0600

    services details

--- Final Project/leaderboard.php
+++ Final Project/leaderboard.php
@@ -24,7 +24,6 @@
 			}
 		?>
 	</div>
-
 	<?php 
 		include 'footer.html'
 	?>
---
+++ Final Project/services.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+		include 'nav.html';
+		include 'search.php';
+	?>
+	<div id="content">
+		<a href="#"></a>
+		<h3>I will fix your TV</h3>
+		<img src="">
+		<p></p>
+	</div>
+	<div id="rightMenu">
+		<p>price $20</p>
+		<button>hire me</button>
+	</div>
+	<?php 
+		include 'footer.html'
+	?>
+</body>
+</html>
