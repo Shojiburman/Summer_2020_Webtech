5c35a1013af11a2654a92ac40c6b9992b0e72eca
unknown <shojiburman@gmail.com>
2020-08-15 12:10:55 +0600

    public home page

---
+++ Final Project/home.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to Protibeshi</title>
+</head>
+<body>
+	<?php 
+		include 'nav.html' 
+	?>
+	<div id="hero_section">
+		<h1></h1>
+		<div>
+			<input type="" name="">
+			<select>
+				<option></option>
+			</select>
+			<button>search</button>
+		</div>
+	</div>
+	<div id="services">
+		<h1>Trending Top Services</h1>
+		<div>
+			<img src="">
+			<h3></h3>
+			<p></p>
+		</div>
+		<div>
+			<img src="">
+			<h3></h3>
+			<p></p>
+		</div>
+		<div>
+			<img src="">
+			<h3></h3>
+			<p></p>
+		</div>
+		<div>
+			<img src="">
+			<h3></h3>
+			<p></p>
+		</div>
+	</div>
+	<div id="job">
+		<h1>Job open for you</h1>
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+	</div>
+	<div id="leaderboard">
+		<div>
+			<a href=""></a>
+			<h3></h3>
+			<p></p>
+		</div>
+		<div>
+			<a href=""></a>
+			<h3></h3>
+			<p></p>
+		</div>
+		<div>
+			<a href=""></a>
+			<h3></h3>
+			<p></p>
+		</div>
+		<div>
+			<a href=""></a>
+			<h3></h3>
+			<p></p>
+		</div>
+	</div>
+	<div id="subscription">
+		<div>
+			<h3></h3>
+			<img src="">
+			<p></p>
+			<ul>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+			<button>buy now</button>
+		</div>
+		<div>
+			<h3></h3>
+			<img src="">
+			<p></p>
+			<ul>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+			<button>buy now</button>
+		</div>
+		<div>
+			<h3></h3>
+			<img src="">
+			<p></p>
+			<ul>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+			<button>buy now</button>
+		</div>
+	</div>
+	<div id="about">
+		<div>
+			<h3></h3>
+			<p></p>
+		</div>
+		<div>
+			<h3></h3>
+			<p></p>
+		</div>
+		<div>
+			<h3></h3>
+			<p></p>
+		</div>
+	</div>
+	<footer>
+		<div>
+			<input type="" name="">
+			<button>subscribe</button>
+		</div>
+		<div>
+			<p></p>
+			<ul>
+				<li><a href=""></a></li>
+				<li><a href=""></a></li>
+				<li><a href=""></a></li>
+			</ul>
+		</div>
+	</footer>
+
+</body>
+</html>
