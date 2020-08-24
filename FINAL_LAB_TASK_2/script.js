c61cd987fc9813a1936929b1b70b754e05248894
unknown <shojiburman@gmail.com>
2020-08-24 23:02:05 +0600

    blood validation

--- FINAL_LAB_TASK_2/script.js
+++ FINAL_LAB_TASK_2/script.js
@@ -149,6 +149,34 @@ window.addEventListener('DOMContentLoaded', (event) => {
         }
 		
 	}, false);
+
+	/*---------------------------------------blood validation---------------------------------*/
+
+	document.getElementById('blood').addEventListener("submit", function(evt) {
+        evt.preventDefault();
+        var form = evt.target;
+        var msg = 'select your blood group';
+       
+
+        var selector = form.querySelector('[name="blood"]');
+    	var value = selector[selector.selectedIndex].value;
+    	console.log(value);
+        if(value == 0) {
+			msg = 'select your blood group';
+		} else {
+			msg = 'Success!';
+		}
+		document.getElementById('bloodformmsg').innerHTML = msg;
+		if(msg != 'Success!') {
+			document.getElementById('bloodformmsg').style.cssText = "display: inline; color: red";
+		} else {
+			document.getElementById('bloodformmsg').style.cssText = "display: inline; color: green";
+		}
+        if (msg == 'Success!') {
+        	// submit form
+        }
+		
+	}, false);
 });
 
 function validateName(string) {
