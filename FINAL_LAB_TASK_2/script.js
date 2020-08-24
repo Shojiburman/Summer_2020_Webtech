316c3e58e1bf5d7cbdafe5247eb5219f4417fb33
unknown <shojiburman@gmail.com>
2020-08-24 11:09:41 +0600

    email validation

--- FINAL_LAB_TASK_2/script.js
+++ FINAL_LAB_TASK_2/script.js
@@ -1,4 +1,6 @@
 window.addEventListener('DOMContentLoaded', (event) => {
+	document.getElementById('nameformmsg').style.cssText = "display: inline; color: red";
+	/*------------------name field validation---------------------------------------------*/
     document.getElementById('name').addEventListener("submit", function(evt) {
         evt.preventDefault();
         var form = evt.target;
@@ -25,10 +27,73 @@ window.addEventListener('DOMContentLoaded', (event) => {
             msg = 'Name can not be empty';
         }
         document.getElementById('nameformmsg').innerHTML = msg;
+        if(msg != 'Success!') {
+        	document.getElementById('nameformmsg').style.cssText = "display: inline; color: red";
+        } else {
+        	document.getElementById('nameformmsg').style.cssText = "display: inline; color: green";
+        }
         if (msg == 'Success!') {
         	// submit form
         }        
     }, false);
+
+
+/*------------------email field validation---------------------------------------------*/
+
+	document.getElementById('email').addEventListener("submit", function(evt) {
+		evt.preventDefault();
+		var form = evt.target;
+		var email = form.querySelector('[name="email"]').value.trim();
+		var msg = '';
+		if (email != ''){
+			msg = "Success!";
+			if (email.indexOf(" ") == -1) {
+				msg = 'Success!';
+				if (multipleAT(email)) {
+					msg = 'Success!';
+					if (email.indexOf("@") > 0) {
+						msg = 'Success!';
+						if (email.indexOf(".") > -1) {
+							msg = 'Success!';
+							var domainExt = email.split("@")[1];
+							var domain = domainExt.split(".")[0];
+							var ext = domainExt.replace(domain, '');
+							console.log(ext);
+							if(domain.length != 0 && validateDomainName(domain)){
+								msg = 'Success!';
+								if(ext.length > 1 && validateDomainExt(ext)){
+									msg = 'Success!';
+								} else {
+									msg = 'Email must have more than 1 letter and letters only after last ".", should not have number.';
+								}
+							} else {
+								msg = 'Email can only have dot(.), dash(-), chracters and numbers between "@" and last "." with no adjacent "@" or "."';
+							}
+						} else {
+							msg = 'Email must have "."';
+						}
+					} else {
+						msg = 'Email can not start with "@"';
+					}
+				} else {
+					msg = 'Email can not have multiple "@"';
+				}
+			} else {
+				msg = 'Email can not have spaces';
+			}
+		} else {
+			msg = 'Email can not be empty';
+		}
+		document.getElementById('emailformmsg').innerHTML = msg;
+		if(msg != 'Success!') {
+			document.getElementById('emailformmsg').style.cssText = "display: inline; color: red";
+		} else {
+			document.getElementById('emailformmsg').style.cssText = "display: inline; color: green";
+		}
+        if (msg == 'Success!') {
+        	// submit form
+        }
+	}, false);
 });
 
 function validateName(string) {
@@ -37,6 +102,48 @@ function validateName(string) {
     array.forEach(function(value) {
         if ((value == '.') || (value == '-') || (value == ' ') || (value.toLowerCase() != value.toUpperCase())) {} else {
             flag = false;
+        }
+    });
+    return flag;
+}
+
+
+function multipleAT(string) {
+    var array = string.split('@');
+    if ( array.length == 2) {
+    	return true
+    }
+    return false;
+}
+
+function validateDomainName(string) {
+  	var array = string.split('');
+    var flag = true;
+    array.forEach(function(value) {
+        if ((value == '')) {
+        	flag = false;
+        } else {
+        	if (value == '-' || value == '.' || ((value.toLowerCase() != value.toUpperCase())) ){} else {
+        		flag = false;
+        	}
+        }
+    });
+    return flag;
+}
+
+function validateDomainExt(string) {
+	var array = string.replace('.','');
+	console.log(array);
+    array = array.split('');
+    console.log(array);
+    var flag = true;
+    array.forEach(function(value) {
+        if (value == ' ') {
+        	flag = false;
+        } else {
+        	if ((value.toLowerCase() != value.toUpperCase())) {} else {
+        		flag = false;
+        	}
         }
     });
     return flag;
