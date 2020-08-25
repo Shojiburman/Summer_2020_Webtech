9397c27c2263fd2025b576ab2b0f24b4691fc488
unknown <shojiburman@gmail.com>
2020-08-25 08:41:25 +0600

    email validation

--- FINAL_LABTASK_3/script.js
+++ FINAL_LABTASK_3/script.js
@@ -35,6 +35,60 @@ window.addEventListener('DOMContentLoaded', (event) => {
         if (msg == 'Success!') {
         	// submit form
         }        
+
+    /*------------------email field validation---------------------------------------------*/
+
+    
+        var email = form.querySelector('[name="email"]').value.trim();
+        var msg = '';
+        if (email != ''){
+            msg = "Success!";
+            if (email.indexOf(" ") == -1) {
+                msg = 'Success!';
+                if (multipleAT(email)) {
+                    msg = 'Success!';
+                    if (email.indexOf("@") > 0) {
+                        msg = 'Success!';
+                        if (email.indexOf(".") > -1) {
+                            msg = 'Success!';
+                            var domainExt = email.split("@")[1];
+                            var domain = domainExt.split(".")[0];
+                            var ext = domainExt.replace(domain, '');
+                            console.log(ext);
+                            if(domain.length != 0 && validateDomainName(domain)){
+                                msg = 'Success!';
+                                if(ext.length > 1 && validateDomainExt(ext)){
+                                    msg = 'Success!';
+                                } else {
+                                    msg = 'Email must have more than 1 letter and letters only after last ".", should not have number.';
+                                }
+                            } else {
+                                msg = 'Email can only have dot(.), dash(-), chracters and numbers between "@" and last "." with no adjacent "@" or "."';
+                            }
+                        } else {
+                            msg = 'Email must have "."';
+                        }
+                    } else {
+                        msg = 'Email can not start with "@"';
+                    }
+                } else {
+                    msg = 'Email can not have multiple "@"';
+                }
+            } else {
+                msg = 'Email can not have spaces';
+            }
+        } else {
+            msg = 'Email can not be empty';
+        }
+        document.getElementById('msg').innerHTML = msg;
+        if(msg != 'Success!') {
+            document.getElementById('msg').style.cssText = "display: inline; color: red";
+        } else {
+            document.getElementById('msg').style.cssText = "display: inline; color: green";
+        }
+        if (msg == 'Success!') {
+            // submit form
+        }
     }, false);
 });
 
@@ -44,6 +98,47 @@ function validateName(string) {
     array.forEach(function(value) {
         if ((value == '.') || (value == '-') || (value == ' ') || (value.toLowerCase() != value.toUpperCase())) {} else {
             flag = false;
+        }
+    });
+    return flag;
+}
+
+function multipleAT(string) {
+    var array = string.split('@');
+    if ( array.length == 2) {
+        return true
+    }
+    return false;
+}
+
+function validateDomainName(string) {
+    var array = string.split('');
+    var flag = true;
+    array.forEach(function(value) {
+        if ((value == '')) {
+            flag = false;
+        } else {
+            if (value == '-' || value == '.' || ((value.toLowerCase() != value.toUpperCase())) ){} else {
+                flag = false;
+            }
+        }
+    });
+    return flag;
+}
+
+function validateDomainExt(string) {
+    var array = string.replace('.','');
+    console.log(array);
+    array = array.split('');
+    console.log(array);
+    var flag = true;
+    array.forEach(function(value) {
+        if (value == ' ') {
+            flag = false;
+        } else {
+            if ((value.toLowerCase() != value.toUpperCase())) {} else {
+                flag = false;
+            }
         }
     });
     return flag;
